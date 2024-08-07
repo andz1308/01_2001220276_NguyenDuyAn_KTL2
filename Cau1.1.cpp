40d36a132bd2241cb03abbd0a326d62b93e0a70b
andz1308 <168605268+andz1308@users.noreply.github.com>
2024-08-07 14:31:30 +0700

    finish Cau1.1

---
+++ Cau1.1.cpp
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+
+int main() {
+	// Khai báo một chuỗi chứa tên
+	char ten[] = "Nguyen Duy An";
+
+	// Xuất chuỗi ra màn hình
+	printf("Ten cua toi la: %s\n", ten);
+
+	return 0;
+}
